c806128cfefbf6ef363218b670adf3dfb849eaff
lachowicz1991 <lachowicz1991@protonmail.com>
2021-07-26 17:14:22 +0100

    in user.py: -add User class constructor -add send_message method in User Class -add print_message method in User Class

--- user.py
+++ user.py
@@ -1,3 +1,13 @@
 
 class User:
-	pass
+	def __init__(self, nickname, password):
+		self.nickname = nickname
+		self.password = password
+		self.messages = []
+
+	def send_message(self, nickname_to, text):
+	def print_messages(self):
+		for message in self.messages:
+			print(f'* {message.from_user} - {message.timestamp}')
+			print(message.text)
+			print()
